64c57df9ee2793604b2b0181a5308133fc8157d0
KingHusbandOficial <kinghusbandoficial@gmail.com>
2022-10-04 13:45:09 -0400

    Adding exercise 1 and 2 (with the knowledge gained to date)

---
+++ script.js
@@ -0,0 +1,44 @@
+/* 1. Ask for the age and if it is more than 18 years old, indicates that can already drive. */
+
+/* let age = prompt(`how old are you?`);
+
+if (Number(age) == age) {
+    if (age >= 18) {
+    document.write(`You can drive.`);
+    } else if (age < 18) {
+    document.write(`You can't drive.`);
+    }
+} else {
+    alert("Introduce a valid number.");
+} */
+
+/* 2. Aks for a score (number). Show a qualification according the score:
+
+0-3: Very poor
+3-5: Insufficient
+5-6: Enough
+6-7: Good
+7-9: Remarkable
+9-10: Outstanding */
+
+/* let score = prompt(`What is your score? (0-10)`);
+
+if (Number(score) == score) {
+    if (score >= 0 && score <= 10) {
+        if (score < 3) {
+            document.write(`Very poor.`);
+        } else if (score < 5) {
+            document.write(`Insufficient.`);
+        } else if (score < 6) {
+            document.write(`Enough.`);
+        } else if (score < 7) {
+            document.write(`Good.`);
+        } else if (score < 9) {
+            document.write(`Remarkable.`);
+        } else if (score >= 9) {
+            document.write(`Outstanding.`);
+        }
+    } else {
+        alert(`Wrong score.`);
+    }
+} */
